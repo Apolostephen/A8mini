0a1b0296e5bd5410c96d89c23f6ad2055f75f610
Apolostephen <627672338@qq.com>
2025-01-06 15:29:55 +0800

    topotek pitch yaw rotation is ok

--- topotek.py
+++ topotek.py
@@ -7,13 +7,22 @@ from tf.transformations import euler_from_quaternion
 import math
 import requests
 import json
+from enum import Enum
 # 创建UDP套接字
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-p_result = 0
 # 绑定本地地址和端口
-ip = "192.168.144.8"
+ip = "192.168.144.92"
 port = 12345
 udp_socket.bind((ip,port))
+
+#发送消息至目标的地址
+addr = ("192.168.144.108",9003) 
+
+#创建命令类型枚举
+class CmdType(Enum):
+    rotation_pitch = 0
+    rotation_yaw = 1
+    rotation_roll = 2
 
 def Add_TPCmd_Crc(cmd,len): #len为cmd的总长度，cmd此时为缺少crc，需要补充上
     crc = 0
@@ -21,11 +30,24 @@ def Add_TPCmd_Crc(cmd,len): #len为cmd的总长度，cmd此时为缺少crc，需
         crc = crc + ord(cmd[i])
     crc = crc % 256
     formatted_crc = "{:02X}".format(crc)
-    print(formatted_crc)
+    #print(formatted_crc)
     cmd += formatted_crc
-    print(cmd)
+    #print(cmd)
     return cmd
 
+def data_append_cmd(data,Cmdtype):
+    if Cmdtype == CmdType.rotation_pitch:
+        data += 'GAP'
+    elif Cmdtype == CmdType.rotation_yaw:
+        data += 'GAY'
+    elif Cmdtype == CmdType.rotation_roll:
+        data += 'GAR'
+    return data
+def data_send(data):
+    cmd = Add_TPCmd_Crc(data,len(data))
+    cmd_bytes = cmd.encode('utf-8')
+    # 发送数据
+    udp_socket.sendto(cmd_bytes, addr)
 def poseCallback(msg):
     quaternion = msg.orientation
     quaternion = msg.orientation
@@ -36,77 +58,49 @@ def poseCallback(msg):
     roll,pitch,yaw = euler_from_quaternion([x, y, z, w])
     pitch = math.degrees(pitch)
     yaw = math.degrees(yaw)
+    roll = math.degrees(roll)
     pitch = pitch*100
     yaw = yaw*100
+    roll = roll*100
     #data = '#tpPG6wGAP 0xFFFF 0xFF crc'
-    data = '#tpPG6w'
+    
     if pitch != 0:
-        data += 'GAP'
-        if pitch < 0:
-            pitch = ~pitch
-            pitch += 1
-            hex_pitch = "{:04X}".format(pitch)
+        data = '#tpPG6w'                                        #数据头 tp--变长命令 P--udp G--云台 6--命令长度为6 w--控制命令
+        data = data_append_cmd(data,CmdType.rotation_pitch)     #添加命令类型，如pitch转动
+                                                                
+        if pitch < 0:           
+            hex_pitch = "{:04X}".format((int(pitch) & 0xffff))
         else:
-            hex_pitch = "{:04X}".format(pitch)
-    data += hex_pitch
-    data += "32"
-    cmd = Add_TPCmd_Crc(data,len(data))
-    cmd_bytes = cmd.encode('utf-8')
-    # 发送数据
-    addr = ("192.168.144.25",37260) #发送消息至目标的地址
-    udp_socket.sendto(cmd_bytes, addr)
+            hex_pitch = "{:04X}".format(int(pitch))
+        data += hex_pitch                                       #添加pitch角度
+        data += "32"#速度,32代表速度为50,即5度每秒                 #添加转动速度
+        data_send(data)                                         #经crc校验后发送数据
+    if yaw != 0:
+        data = '#tpPG6w'
+        data = data_append_cmd(data,CmdType.rotation_yaw)
 
-def captureImageCallback(msg):
-    data = [0x55,0x66,0x01,0x01,0x00,0x00,0x00,0x0c,0x00,0x34,0xce] #
-    # crc_check_16bites(data, len(data))
-    # high = (p_result & 0xff00) >> 8
-    # low = p_result & 0x00FF
-    # data.append(low)
-    # data.append(high)
-    # 发送数据
-    addr = ("192.168.144.25",37260) #发送消息至目标的地址
-    data_bytes = bytes([x & 0xFF for x in data])
-    udp_socket.sendto(data_bytes,addr)
-    data.clear()
-    look_for_photo()
-def fetch_data_from_url(url, params):
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()  # 检查请求是否成功
-        data = response.json()  # 假设返回的是 JSON 数据
-        print("请求成功！！")
-        json_data = json.dumps(data, indent=4, ensure_ascii=False)
-        print(json_data)
-    except requests.exceptions.RequestException as e:
-        print(f"请求失败: {e}")
-        return None
-def look_for_photo():
-    url_getdirectories = "http://192.168.144.25:82//cgi-bin/media.cgi/api/v1/getdirectories"#获取文件夹目录列表
-    url_getmediacount = "http://192.168.144.25:82//cgi-bin/media.cgi/api/v1/getmediacount" #获取文件数量
-    url_getmedialist = "http://192.168.144.25:82//cgi-bin/media.cgi/api/v1/getmedialist" #获取文件列表
-    params_getdirectories = {
-        "media_type": 0
-    }
-    params_getmediacount = {
-        "media_type": 0,
-        "path": "A"
-    }
-    params_getmedialist = {
-        "media_type": 0,
-        "path": "A",
-        "start": 0,
-        "count": 10
-    }
-    fetch_data_from_url(url_getmedialist,params_getmedialist)
+        if yaw < 0:
+            hex_yaw = "{:04X}".format(int(yaw) & 0xffff)
+        else:
+            hex_yaw = "{:04X}".format(int(yaw))
+        data += hex_yaw
+        data += '32'
+        data_send(data)
+    if roll != 0:
+        data = '#tpPG6w'                                      
+        data = data_append_cmd(data,CmdType.rotation_roll)
+
+        if roll < 0:
+            hex_roll = "{:04X}".format(int(roll) & 0xffff)
+        else:
+            hex_roll = "{:04X}".format(int(roll))
+        data += hex_roll
+        data += '32'
+        data_send(data)
+
 if __name__ == '__main__':
-
     rospy.init_node('A8mini')
-    # data = '#TPUD2wAWB01'
-    # '#tpPG6wGAP 0xFFFF 0xFF crc'#GAP为云台 角度 pitch 控制 还有GAY GAR,前四个十六进制代表转动角度，后两个十六进制代表速度，精度为0.01
-    # Add_TPCmd_Crc(data,len(data))
     rospy.Subscriber("/A8mini", Pose, poseCallback)
-    rospy.Subscriber("/A8mini/captureImage", Empty, captureImageCallback)
-    #data = [0x55,0x66,0x01,0x09,0x00,0x00,0x00,0x21,0x00,0x02,0x00,0x0f,0x70,0x08,0x98,0x3a,0x00,0x70,0xbe]
     rospy.spin()
 
         
---
+++ topotek_send.py
@@ -0,0 +1,35 @@
+import rospy
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Quaternion
+from tf.transformations import quaternion_from_euler
+from std_msgs.msg import Empty
+import math
+def euler_to_quaternion(roll, pitch, yaw):
+    quaternion = quaternion_from_euler(roll, pitch, yaw)
+    return Quaternion(*quaternion)
+
+def send_pose():
+    
+
+    roll = 0
+    pitch = float(input("请输入Pitch角度："))
+    yaw = float(input("请输入Yaw角度："))
+    pitch = math.radians(pitch)
+    yaw = math.radians(yaw)
+    pose_msg = Pose()
+    pose_msg.orientation = euler_to_quaternion(roll, pitch, yaw)
+    pub.publish(pose_msg)
+      # 发送频率为10Hz
+def capture_image():
+    input("按下回车键")
+    pub.publish(Empty())
+if __name__ == '__main__':
+    rospy.init_node('pose_sender', anonymous=True)
+    pub = rospy.Publisher('/A8mini', Pose, queue_size=10)
+    while True:
+        try:
+            send_pose()
+            rate = rospy.Rate(10)
+            rate.sleep()
+        except rospy.ROSInterruptException:
+            pass
